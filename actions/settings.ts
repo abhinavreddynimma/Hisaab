9fb5c6a492ef9c783efa525ca9c2cc2c2d67ec27
abhinavreddynimma <abhinavreddynimma@gmail.com>
2023-04-09 17:00:33 +0530

    Add settings page and data migration utilities - change - supporting files

---
+++ actions/settings.ts
@@ -0,0 +1,102 @@
+"use server";
+
+import { db } from "@/db";
+import { settings } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import type { UserProfile, LeavePolicy, InvoiceSettings } from "@/lib/types";
+
+export async function getSetting<T>(key: string): Promise<T | null> {
+  const row = db.select().from(settings).where(eq(settings.key, key)).get();
+  if (!row) return null;
+  return JSON.parse(row.value) as T;
+}
+
+export async function setSetting(key: string, value: unknown): Promise<void> {
+  const jsonValue = JSON.stringify(value);
+  const existing = db.select().from(settings).where(eq(settings.key, key)).get();
+  if (existing) {
+    db.update(settings).set({ value: jsonValue }).where(eq(settings.key, key)).run();
+  } else {
+    db.insert(settings).values({ key, value: jsonValue }).run();
+  }
+}
+
+export async function getUserProfile(): Promise<UserProfile> {
+  const profile = await getSetting<UserProfile>("user_profile");
+  return profile ?? {
+    name: "",
+    company: "",
+    addressLine1: "",
+    addressLine2: "",
+    city: "",
+    state: "",
+    pincode: "",
+    country: "",
+    gstin: "",
+    pan: "",
+    email: "",
+    phone: "",
+    bankName: "",
+    bankAccount: "",
+    bankIfsc: "",
+    bankBranch: "",
+    bankIban: "",
+    bankBic: "",
+    sepaAccountName: "",
+    sepaIban: "",
+    sepaBic: "",
+    sepaBank: "",
+    sepaAccountType: "",
+    sepaAddress: "",
+    swiftAccountName: "",
+    swiftIban: "",
+    swiftBic: "",
+    swiftBank: "",
+    swiftAccountType: "",
+  };
+}
+
+export async function saveUserProfile(profile: UserProfile): Promise<{ success: boolean }> {
+  await setSetting("user_profile", profile);
+  return { success: true };
+}
+
+export async function getLeavePolicy(): Promise<LeavePolicy> {
+  const policy = await getSetting<LeavePolicy>("leave_policy");
+  return policy ?? {
+    leavesPerMonth: 1,
+    standardWorkingDays: 22,
+    trackingStartDate: "2025-05",
+  };
+}
+
+export async function saveLeavePolicy(policy: LeavePolicy): Promise<{ success: boolean }> {
+  await setSetting("leave_policy", policy);
+  return { success: true };
+}
+
+export async function getInvoiceSettings(): Promise<InvoiceSettings> {
+  const invoiceSettings = await getSetting<InvoiceSettings>("invoice_settings");
+  return invoiceSettings ?? {
+    prefix: "INV",
+    nextNumber: 1,
+    defaultHsnSac: "998314",
+    defaultTaxRate: 18,
+    taxType: "cgst_sgst",
+  };
+}
+
+export async function saveInvoiceSettings(invoiceSettingsData: InvoiceSettings): Promise<{ success: boolean }> {
+  await setSetting("invoice_settings", invoiceSettingsData);
+  return { success: true };
+}
+
+export async function getDefaultProjectId(): Promise<number | null> {
+  const setting = await getSetting<{ projectId: number | null }>("default_project");
+  return setting?.projectId ?? null;
+}
+
+export async function saveDefaultProjectId(projectId: number | null): Promise<{ success: boolean }> {
+  await setSetting("default_project", { projectId });
+  return { success: true };
+}
---
+++ app/settings/page.tsx
@@ -0,0 +1,48 @@
+import {
+  getUserProfile,
+  getLeavePolicy,
+  getInvoiceSettings,
+} from "@/actions/settings";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { UserDetailsForm } from "@/components/settings/user-details-form";
+import { LeavePolicyForm } from "@/components/settings/leave-policy-form";
+import { InvoiceSettingsForm } from "@/components/settings/invoice-settings-form";
+
+export default async function SettingsPage() {
+  const [userProfile, leavePolicy, invoiceSettings] = await Promise.all([
+    getUserProfile(),
+    getLeavePolicy(),
+    getInvoiceSettings(),
+  ]);
+
+  return (
+    <div className="space-y-6">
+      <div>
+        <h1 className="text-3xl font-bold tracking-tight">Settings</h1>
+        <p className="text-muted-foreground">
+          Manage your profile, leave policy, and invoice preferences.
+        </p>
+      </div>
+
+      <Tabs defaultValue="profile" className="space-y-6">
+        <TabsList>
+          <TabsTrigger value="profile">Profile</TabsTrigger>
+          <TabsTrigger value="leave-policy">Leave Policy</TabsTrigger>
+          <TabsTrigger value="invoice-settings">Invoice Settings</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="profile">
+          <UserDetailsForm initialData={userProfile} />
+        </TabsContent>
+
+        <TabsContent value="leave-policy">
+          <LeavePolicyForm initialData={leavePolicy} />
+        </TabsContent>
+
+        <TabsContent value="invoice-settings">
+          <InvoiceSettingsForm initialData={invoiceSettings} />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
