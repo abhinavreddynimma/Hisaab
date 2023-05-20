f4c90a381079294fb5a2214b4bc00fac0f1b5f43
abhinavreddynimma <abhinavreddynimma@gmail.com>
2023-05-20 20:37:48 +0530

    Redesign UI with modern color scheme, typography, and c - app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Inter, Plus_Jakarta_Sans } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/layout/theme-provider"
 import { AppShell } from "@/components/layout/app-shell"
 import { Toaster } from "sonner"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-display",
+  weight: ["500", "600", "700", "800"],
+})
 
 export const metadata: Metadata = {
-  title: "Payroll App",
+  title: "Hisaab",
   description: "Freelancer Payroll Management",
 }
 
@@ -19,10 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`${inter.variable} ${jakarta.variable} font-sans`}>
         <ThemeProvider>
           <AppShell>{children}</AppShell>
-          <Toaster />
+          <Toaster richColors />
         </ThemeProvider>
       </body>
     </html>
