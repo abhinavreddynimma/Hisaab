e29039be187dd223351b18e90021afd0a2bfdff7
abhinavreddynimma <abhinavreddynimma@gmail.com>
2023-08-19 10:43:14 +0530

    Add Docker support for self-hosting - components/ui (part 1)

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl border border-border/60 bg-card text-card-foreground shadow-sm shadow-black/[0.04]",
+      "rounded-xl border border-border bg-card text-card-foreground shadow",
       className
     )}
     {...props}
@@ -35,7 +35,7 @@ const CardTitle = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("font-display font-semibold leading-none tracking-tight", className)}
+    className={cn("font-semibold leading-none tracking-tight", className)}
     {...props}
   />
 ))
