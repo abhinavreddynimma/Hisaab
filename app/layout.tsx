a170dbd615d201ad572b1fb2b344f56d781ef2aa
Abhinav Reddy Nimma <abhinavreddynimma@gmail.com>
2023-12-30 14:55:12 +0530

    Add demo tooling and calendar UI refinements - app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,33 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
+import { Inter, Plus_Jakarta_Sans } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/layout/theme-provider"
 import { AppShell } from "@/components/layout/app-shell"
 import { Toaster } from "sonner"
-import { getAuthContext } from "@/lib/auth"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" })
+const jakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-jakarta",
+  weight: ["500", "600", "700", "800"],
+})
 
 export const metadata: Metadata = {
-  title: "Payroll App",
+  title: "Hisaab",
   description: "Freelancer Payroll Management",
 }
 
-export const dynamic = "force-dynamic"
-
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const authState = await getAuthContext()
-
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={`${inter.variable} ${jakarta.variable} font-sans`}>
         <ThemeProvider>
-          <AppShell authState={authState}>{children}</AppShell>
-          <Toaster />
+          <AppShell>{children}</AppShell>
+          <Toaster richColors />
         </ThemeProvider>
       </body>
     </html>
