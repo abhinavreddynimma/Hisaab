f488e44a3e4a65e76dd19ef1c08f5ce30ba1c2d1
Conductor <conductor@noreply>
2026-01-11 17:49:34 +0530

    Saving uncommitted changes before archiving - remaining - drizzle

--- drizzle/0012_bank_statement_dismiss.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `bank_statement_entries` ADD COLUMN `is_dismissed` integer DEFAULT false NOT NULL;
--- drizzle/0013_phonepe_fields.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `bank_statement_entries` ADD COLUMN `time` text;
-ALTER TABLE `bank_statement_entries` ADD COLUMN `phonepe_name` text;
